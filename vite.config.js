4092f686ab0ec9d48ed6ceda8cc3f7f5085c7a6c
Dimitar871 <180311631+Dimitar871@users.noreply.github.com>
2025-06-18 00:08:24 +0200

    Update vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-        rollupOptions: {
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-        },
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        tailwindcss(),
     ],
 });
